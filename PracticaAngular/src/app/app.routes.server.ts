de918f515f410231f612d4adc6a2de0c85f9d461
Marcos Jesús Ríos Duran <230733@utxicotepec.edu.mx>
2025-02-24 12:54:26 -0600

    Update app.routes.server.ts

--- PracticaAngular/src/app/app.routes.server.ts
+++ PracticaAngular/src/app/app.routes.server.ts
@@ -1,27 +1,14 @@
-import { RenderMode, ServerRoute } from '@angular/ssr';
-import { Page1Component } from './components/page-container/pages/page1/page1.component';
-import { Page2Component } from './components/page-container/pages/page2/page2.component';
-import { Page3Component } from './components/page-container/pages/page3/page3.component';
+import { mergeApplicationConfig, ApplicationConfig } from '@angular/core';
+import { provideServerRendering } from '@angular/platform-server';
+import { provideServerRoutesConfig } from '@angular/ssr';
+import { appConfig } from './app.config';
+import { serverRoutes } from './app.routes.server';
 
-export const serverRoutes: ServerRoute[] = [
-  {
-    path: 'page1',
-    component: Page1Component,
-    renderMode: RenderMode.Prerender
-  },
-  {
-    path: 'page2',
-    component: Page2Component,
-    renderMode: RenderMode.Prerender
-  },
-  {
-    path: 'page3',
-    component: Page3Component,
-    renderMode: RenderMode.Prerender
-  },
-  {
-    path: '**',
-    redirectTo: '/page1', // Redirección con ruta válida
-    renderMode: RenderMode.Prerender
-  }
-];
+const serverConfig: ApplicationConfig = {
+  providers: [
+    provideServerRendering(),
+    provideServerRoutesConfig(serverRoutes)
+  ]
+};
+
+export const config = mergeApplicationConfig(appConfig, serverConfig);
